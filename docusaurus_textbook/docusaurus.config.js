23e4616efc41939ef7692c9b5e9c0660013cf264
Subhan Kaladi <153628962+subhankaladi@users.noreply.github.com>
2025-11-30 02:40:38 +0500

    Update docusaurus.config.js

--- docusaurus_textbook/docusaurus.config.js
+++ docusaurus_textbook/docusaurus.config.js
@@ -7,19 +7,19 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  // url: 'https://your-github-username.github.io',
-  // // Set the /<baseUrl>/ pathname under which your site is served
-  // // For GitHub pages deployment, it is often '/<projectName>/'
-  // baseUrl: '/physical-ai-humanoid-robotics-textbook/',
+  url: 'https://your-github-username.github.io',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/physical-ai-humanoid-robotics-textbook/',
 
-  // // GitHub pages deployment config.
-  // // If you aren't using GitHub pages, you don't need these.
-  // organizationName: 'your-github-username', // Usually your GitHub org/user name.
-  // projectName: 'physical-ai-humanoid-robotics-textbook', // Usually your repo name.
-  // deploymentBranch: 'gh-pages',
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'your-github-username', // Usually your GitHub org/user name.
+  projectName: 'physical-ai-humanoid-robotics-textbook', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
 
-  // onBrokenLinks: 'throw',
-  // onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
