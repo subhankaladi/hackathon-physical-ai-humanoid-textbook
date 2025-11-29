a14a2d25b45f610f9bd876a06d45626fc490c100
Subhan Kaladi <153628962+subhankaladi@users.noreply.github.com>
2025-11-30 02:49:13 +0500

    Update docusaurus.config.js

--- docusaurus_textbook/docusaurus.config.js
+++ docusaurus_textbook/docusaurus.config.js
@@ -7,7 +7,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-github-username.github.io',
+  url: 'https://subhankaladi.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/physical-ai-humanoid-robotics-textbook/',
